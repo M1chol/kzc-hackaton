3a97e530f9e068f44595ddd2bf276c249e9d5c7c
Fred <64082311+MatiP911@users.noreply.github.com>
2023-12-02 12:46:36 +0100

    Smg chenge again in that stupid file, i don't remember what

--- aigpt.py
+++ aigpt.py
@@ -1,43 +1,43 @@
 import openai
 from keys import OPENAI_API_KEY
 
-# OPENAI_API_KEY = os.getenv('GPT_API_KEY')
+class chatapigpt():
+    def __init__(self) -> None:
+        self.client = openai.OpenAI(api_key=OPENAI_API_KEY)
 
+        #TODO Temp switch
+        file_path = './test/programTestAi.txt'
 
-client = openai.OpenAI(api_key=OPENAI_API_KEY)
+        with open(file_path, 'r') as file:
+            self.fileContent = file.read()
+        
+    def contentMachine(self, fileContent, title) -> str:
+        completion1 = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "Your job is to give part of code and make short documentation of code given. Say OK if you understood."},
+                {"role": "assistant", "content": "OK"},
+                {"role": "user", "content": "Title of file is: " + title + "\n\nCode is:" + self.fileContent}
+            ]
+        )
 
-file_path = './test/programTestAi.txt'
+        completion2 = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "Your job is to link parts of code in snippets with relevent parts of documentation. Say OK if you understood."},
+                {"role": "assistant", "content": "OK"},
+                {"role": "user", "content": '\n\nCode:\n' + self.fileContent + '\n\nDocumentation:\n' + completion1.choices[0].message.content}
+            ]
+        )
 
-with open(file_path, 'r') as file:
-    fileContent = file.read()
+        completion3 = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "Your job is to convert given documentation into one raw markdown file. Say OK if you understood."},
+                {"role": "assistant", "content": "OK"},
+                {"role": "user", "content": completion2.choices[0].message.content}
+            ]
+        )
 
-completion1 = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "Your job is to give part of code and make short documentation of code given. Say OK if you understood."},
-        {"role": "assistant", "content": "OK"},
-        {"role": "user", "content": fileContent}
-    ]
-)
-
-completion2 = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "Your job is to link parts of code in snippets with relevent parts of documentation. Say OK if you understood."},
-        {"role": "assistant", "content": "OK"},
-        {"role": "user", "content": '\n\nCode:\n' + fileContent + '\n\nDocumentation:\n' + completion1.choices[0].message.content}
-    ]
-)
-
-completion3 = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "Your job is to convert given documentation into one raw markdown file. Say OK if you understood."},
-        {"role": "assistant", "content": "OK"},
-        {"role": "user", "content": completion2.choices[0].message.content}
-    ]
-)
-
-content = completion3.choices[0].message.content
-print(type(content))
-print(content)
+        content = completion3.choices[0].message.content
+        return content
