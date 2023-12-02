2a575b4416d9f6e86d393c7882e13a0fd99e4f82
Fred <64082311+MatiP911@users.noreply.github.com>
2023-12-02 14:08:00 +0100

    Probably working class, i hope

--- aigpt.py
+++ aigpt.py
@@ -5,39 +5,42 @@ class chatapigpt():
     def __init__(self) -> None:
         self.client = openai.OpenAI(api_key=OPENAI_API_KEY)
 
-        #TODO Temp switch
-        file_path = './test/programTestAi.txt'
+        # #TODO Temp switch
+        # file_path = './test/programTestAi.txt'
 
-        with open(file_path, 'r') as file:
-            self.fileContent = file.read()
+        # with open(file_path, 'r') as file:
+        #     self.fileContent = file.read()
         
-    def contentMachine(self, fileContent, title) -> str:
+    def contentMachine(self, title, code) -> str:
         completion1 = self.client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "Your job is to give part of code and make short documentation of code given. Say OK if you understood."},
                 {"role": "assistant", "content": "OK"},
-                {"role": "user", "content": "Title of file is: " + title + "\n\nCode is:" + self.fileContent}
+                {"role": "user", "content": "Title of file is: " + title + "\n\nCode is:" + code}
             ]
         )
 
         completion2 = self.client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "Your job is to link parts of code in snippets with relevent parts of documentation. Say OK if you understood."},
-                {"role": "assistant", "content": "OK"},
-                {"role": "user", "content": '\n\nCode:\n' + self.fileContent + '\n\nDocumentation:\n' + completion1.choices[0].message.content}
+                {"role": "system", "content": "Your job is to add to parts of documentation corresponding code snippets."},
+                # {"role": "assistant", "content": "OK"},
+                {"role": "user", "content": "Code:\n" + code + "\n\nDocumentation:\n" + completion1.choices[0].message.content}
             ]
         )
 
-        completion3 = self.client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "Your job is to convert given documentation into one raw markdown file. Say OK if you understood."},
-                {"role": "assistant", "content": "OK"},
-                {"role": "user", "content": completion2.choices[0].message.content}
-            ]
-        )
+        # completion3 = self.client.chat.completions.create(
+        #     model="gpt-3.5-turbo",
+        #     messages=[
+        #         {"role": "system", "content": "Your job is to convert given documentation into one raw markdown file. Say OK if you understood."},
+        #         {"role": "assistant", "content": "OK"},
+        #         {"role": "user", "content": completion2.choices[0].message.content}
+        #     ]
+        # )
 
-        content = completion3.choices[0].message.content
-        return content
+        return completion2.choices[0].message.content
+
+if __name__ == "__main__":
+    test = chatapigpt()
+    # print(chatapigpt.contentMachine(test, "main.py"))
