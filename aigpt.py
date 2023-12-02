a0af2075bc3b2f37898d5f79573b60922d0528cb
Fred <64082311+MatiP911@users.noreply.github.com>
2023-12-02 10:46:18 +0100

    It returns shit, but wrong shit

--- aigpt.py
+++ aigpt.py
@@ -4,13 +4,20 @@ from keys import OPENAI_API_KEY
 
 client = openai.OpenAI(api_key=OPENAI_API_KEY)
 
+file_path = './test/programTestAi.txt'
+
+with open(file_path, 'r') as file:
+    fileContent = file.read()
+
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "Your job is to make documentation from code given by he user. Say OK if you understood."},
+        {"role": "system", "content": "Your job is to make documentation in markdown format from code given by he user. Say OK if you understood."},
         {"role": "assistant", "content": "OK"},
-        {"role": "user", "content": ""}
+        {"role": "user", "content": fileContent}
     ]
 )
 
-print(completion)
+# print(completion["choices"][0]["message"]["content"])
+content = completion.choices[0].message.content
+print(content)
