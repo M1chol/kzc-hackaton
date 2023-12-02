87069793269480566401fdeb61fbb1c1948f7c9c
Michał Kozłowski <micho.kozlowski@gmail.com>
2023-12-02 14:13:41 +0100

    server not responding pls fix

---
+++ api.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
+from aigpt import chatapigpt
+
+app_api = FastAPI()
+
+# CORS (Cross-Origin Resource Sharing) configuration
+origins = ["http://127.0.0.1:5000"]  # Update this with the actual origin of your Flask app
+
+app_api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+messageList = []
+
+test = {"raw": "",
+ "html": ""};
+
+@app_api.post('/api/send-text')
+def send_text_api(text: str = Form(...)):
+    GPT_Instance = chatapigpt()
+    raw = GPT_Instance.contentMachine(GPT_Instance, text["text"])
+    # wywołanie funkcji basi
+    html = "html"
+
+    return {"raw": raw, "html":html}
--- static/base.js
+++ static/base.js
@@ -1,2 +1,20 @@
-document.getElementById('welcome').innerText += 
-" Editors";
+var textInput = document.getElementById("textInput")
+var textTitle = document.getElementById("textFilename")
+var ip = location.host;
+
+function sendText(){
+    fetch('http://localhost:8000/api/send-text', {
+        method: 'POST',
+        body: new URLSearchParams({
+            text: textInput.value,
+            title: textTitle.value
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.message);
+    })
+    .catch(error => {
+        console.error('Error sending text:', error);
+    });
+}
