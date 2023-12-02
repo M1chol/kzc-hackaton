739f34baa8d9a0ddba269d518f54e06faba1cfd7
Fred <64082311+MatiP911@users.noreply.github.com>
2023-12-02 10:12:16 +0100

    Specified assistend his place in the world

--- aigpt.py
+++ aigpt.py
@@ -7,11 +7,10 @@ client = openai.OpenAI(api_key=OPENAI_API_KEY)
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are assistant that likes to joke to much."},
-        {"role": "user", "content": "How many ducks can fit in larges lake on earth?"}
+        {"role": "system", "content": "Your job is to make documentation from code given by he user. Say OK if you understood."},
+        {"role": "assistant", "content": "OK"},
+        {"role": "user", "content": ""}
     ]
 )
-
-
 
 print(completion)
