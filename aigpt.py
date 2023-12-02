cb979fc5a1030a3cba17ecd3da6f9ff9849619dc
Fred <64082311+MatiP911@users.noreply.github.com>
2023-12-02 15:17:44 +0100

    few quality of life changes

--- aigpt.py
+++ aigpt.py
@@ -4,12 +4,6 @@ from keys import OPENAI_API_KEY
 class chatapigpt():
     def __init__(self) -> None:
         self.client = openai.OpenAI(api_key=OPENAI_API_KEY)
-
-        # #TODO Temp switch
-        # file_path = './test/programTestAi.txt'
-
-        # with open(file_path, 'r') as file:
-        #     self.fileContent = file.read()
         
     def contentMachine(self, title, code) -> str:
         completion = self.client.chat.completions.create(
@@ -24,4 +18,3 @@ class chatapigpt():
 
 if __name__ == "__main__":
     test = chatapigpt()
-    # print(chatapigpt.contentMachine(test, "main.py"))
--- markdownFunction.py
+++ markdownFunction.py
@@ -2,4 +2,3 @@ import markdown
 
 def MarkToHtml(tempMD: str) -> str:
     return markdown.markdown(tempMD)
-
